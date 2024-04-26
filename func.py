e79a8cf765e70d1b50b59119a41d406e6a71de8c
jun <129544812+juniscoding@users.noreply.github.com>
2024-04-26 15:47:13 -0300

    Initial commit

---
+++ func.py
@@ -0,0 +1,25 @@
+import os
+import tweepy
+import json
+import random
+
+with open('quotes.json') as quotes:
+    data = json.load(quotes)
+
+
+bot = tweepy.Client(
+    #Consumer Keys
+    consumer_key= os.environ['CONSUMER_KEY'],
+    consumer_secret= os.environ['CONSUMER_SECRET'],
+    # Access Token and Secret
+    access_token= os.environ['ACCESS_TOKEN'],
+    access_token_secret= os.environ['ACCESS_TOKEN_SECRET'])
+
+def post_quote():
+	book = random.choice(list(data.keys()))
+	book_quotes = data[book]
+	random_index = random.randint(0, len(book_quotes)-1)
+	quote = book_quotes[random_index]
+	r = bot.create_tweet(text=quote)
+	print(book)
+	return None

05d14b9097a7eed9870910a653f76f60bfe0758e
Jun T.N <jun16@ov.ufrj.br>
2024-05-02 13:30:39 -0300

    new func.py

---
+++ func.py
@@ -0,0 +1,28 @@
+import os
+import tweepy
+import json
+import random
+
+with open('quotes.json') as quotes:
+    data = json.load(quotes)
+
+
+bot = tweepy.Client(
+    #Consumer Keys
+    consumer_key= os.environ['CONSUMER_KEY'],
+    consumer_secret= os.environ['CONSUMER_SECRET'],
+    # Access Token and Secret
+    access_token= os.environ['ACCESS_TOKEN'],
+    access_token_secret= os.environ['ACCESS_TOKEN_SECRET'])
+
+def post_quote():
+	quotes = data["verses"]
+	random_index = random.randint(0, len(quotes)-1)
+	quote = quotes[random_index]
+	try:
+		r = bot.create_tweet(text=quote)
+	except:
+		random_index = random.randint(0, len(quotes)-1)
+		quote = quotes[random_index]
+		r = bot.create_tweet(text=quote)
+	return None
